84826fc6af46202e74b795dab87b60ad232ac851
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-03-06 17:23:04 +0530

    added features page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Hero } from "./Components/HeroPage";
+import ExpandablePanels from "./Components/Solution";
 function App(){
   return(
     <>
       <Hero />
+      <ExpandablePanels />
     </>
   )
 }
